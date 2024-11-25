1c29d358745e35da06e57cf74280d6a40971fb54
Espen-ChamplainCollege <espen.wold@mymail.champlain.edu>
2024-11-25 17:48:58 -0500

    Adding file structure

---
+++ src/components/CompositeElement.java
@@ -0,0 +1,7 @@
+/*
+ Ileigh Aube, Alexander Mayo, Espen Wold
+ CSI 340 Final Project: Electronic Secretary
+ */
+
+package component;
+
---
+++ src/components/LeafElement.java
@@ -0,0 +1,6 @@
+/*
+ Ileigh Aube, Alexander Mayo, Espen Wold
+ CSI 340 Final Project: Electronic Secretary
+ */
+
+package component;
